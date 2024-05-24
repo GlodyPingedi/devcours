5ce1bcdeee2aa3dd2f350fcd653faaefe2d14a18
yoshi <glodykabengele55@gmail.com>
2024-05-24 15:08:43 +0100

    Initialisation du projet

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categorie;
+use App\Http\Requests\StoreCategorieRequest;
+use App\Http\Requests\UpdateCategorieRequest;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Categorie::latest()->get();
+        return view('categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategorieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategorieRequest $request, Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categorie $category)
+    {
+        $categorie = $category;
+        $categorie->delete();
+        return redirect()->route('categories.index')->with('success', 'Categorie supprimée avec succès');
+    }
+}
